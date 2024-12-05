b9b8de5a627504a4d5b02c566718bfe06c408cbe
Sanjeetha Sekar <sanjeethasekar08@gmail.com>
2024-12-05 20:42:20 +0530

    fifthdaycommit

---
+++ src/InterviewQuestions/removeduplicatefromstring.java
@@ -0,0 +1,20 @@
+package InterviewQuestions;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class removeduplicatefromstring {
+	
+	public static void main(String[] args)
+	{
+		String s ="Sanjeetha";
+		char c[]=s.toCharArray();
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/InterviewQuestions/stringmanipulations.java
@@ -0,0 +1,15 @@
+package InterviewQuestions;
+
+public class stringmanipulations {
+
+	public static void main(String[] args) {
+		
+		String s ="Selenium is very popular";
+		System.out.println(s.charAt(4));
+		System.out.println(s.length());
+		System.out.println(s.replace("s", "k"));
+		String ss =s.trim();
+		System.out.println(ss);
+	}
+
+}
